78d4f3c46e5c6aad376425e6888d62756ca09750
ernestasambrozas <ernestasambrozas@gmail.com>
2023-12-28 09:12:50 -0500

    Refactor nav links

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,55 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
     const navLinks = document.querySelectorAll('.nav-link');
     const sections = document.querySelectorAll('section');
-    const navbar = document.querySelector('.navbar');
 
-    // Function to set the active link based on the section ID
     function setActiveLink(newActiveId) {
         navLinks.forEach(link => {
             link.classList.remove('active');
-            if (link.href.includes(newActiveId)) {
+            if (link.getAttribute('href').includes(newActiveId)) {
                 link.classList.add('active');
             }
         });
     }
 
-    // Scroll event handler
-    function onScroll() {
-        let currentId = '';
-        let atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
-
-        if (atBottom) {
-            // If at the bottom of the page, set the contact link as active
-            setActiveLink('contact');
-        } else {
-            // Otherwise, determine the active section based on scroll position
-            sections.forEach(section => {
-                const sectionTop = section.offsetTop;
-                const threshold = window.pageYOffset + window.innerHeight * 0.25;
-
-                if (sectionTop <= threshold) {
-                    currentId = section.getAttribute('id');
-                }
-            });
-
-            if (currentId) {
-                setActiveLink(currentId);
-            }
-        }
-    }
-
-    // Add click event listeners to nav links
     navLinks.forEach(link => {
         link.addEventListener('click', function () {
-            // Remove active class from all links and set it on the clicked link
             navLinks.forEach(lnk => lnk.classList.remove('active'));
             link.classList.add('active');
         });
     });
 
-    // Attach the scroll event listener
-    window.addEventListener('scroll', onScroll);
+    function updateNavLinksOnScroll() {
+        let currentId = '';
 
-    // Call onScroll immediately to set initial active state
-    onScroll();
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.offsetHeight;
+            const sectionMiddle = sectionTop + sectionHeight / 2; // Calculate the middle of the section
+            const scrollPosition = window.pageYOffset + window.innerHeight / 2; // Middle of the viewport
+
+            // Check if the middle of the viewport is within the middle half of the section
+            if (scrollPosition > sectionTop + sectionHeight * 0.25 && scrollPosition < sectionTop + sectionHeight * 0.75) {
+                currentId = section.getAttribute('id');
+            }
+        });
+
+        // Check if the user has scrolled to the bottom of the page
+        if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight && currentId !== 'contact') {
+            currentId = 'contact';
+        }
+
+        if (currentId) {
+            setActiveLink(currentId);
+        }
+    }
+
+    window.addEventListener('scroll', updateNavLinksOnScroll);
+    updateNavLinksOnScroll(); // Initialize active link on page load
 });
